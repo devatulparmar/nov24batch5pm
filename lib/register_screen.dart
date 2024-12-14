5a488544f1a43fb2cf0cb1d52bf144fa29782e67
devatulparmar <dev.atulparmar@gmail.com>
2024-12-14 18:00:41 +0530

    Image picker, Switch widget,  Slider widget

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:nov24batch5pm/utils/constants.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -17,12 +21,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
   String maritalStatusString = 'U';
   String? dropDownSelectedCityValue;
   bool isTermsAndConditionAccepted = false;
+  bool isWhatsAppUpdatesEnable = false;
   List<String> cityNamesListObject = [
     "Vadodara",
     "Ahmedabad",
     "Surat",
     "Rajkot"
   ];
+  XFile? selectedImage;
+  double ageSliderValue = 18;
+  List<XFile?> selectedMultiImage = [];
 
   List<DropdownMenuItem> cityList = const [
     DropdownMenuItem(
@@ -43,9 +51,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
     ),
   ];
 
-  bool _validateRegisterForm(){
-    if(formKey.currentState!.validate()){
-      if(genderSelection == 0){
+  bool _validateRegisterForm() {
+    if (formKey.currentState!.validate()) {
+      if (genderSelection == 0) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             content: Text('Please select Gender.'),
@@ -55,8 +63,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
             duration: Duration(seconds: 2),
           ),
         );
-      }
-      else if(maritalStatus == 0){
+      } else if (maritalStatus == 0) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             content: Text('Please select Marital Status.'),
@@ -66,8 +73,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
             duration: Duration(seconds: 2),
           ),
         );
-      }
-      else if(dropDownSelectedCityValue == null){
+      } else if (dropDownSelectedCityValue == null) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             content: Text('Please select City.'),
@@ -77,8 +83,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
             duration: Duration(seconds: 2),
           ),
         );
-      }
-      else if(isTermsAndConditionAccepted == false){
+      } else if (isTermsAndConditionAccepted == false) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             content: Text('Please Accept Terms And Condition.'),
@@ -88,12 +93,43 @@ class _RegisterScreenState extends State<RegisterScreen> {
             duration: Duration(seconds: 2),
           ),
         );
-      }
-      else{
+      } else {
         return true;
       }
     }
     return false;
+  }
+
+  void _pickImageFromGallery() async {
+    selectedImage = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 100,
+      requestFullMetadata: true,
+    );
+    setState(() {});
+  }
+
+  void _pickImageFromCamera() async {
+    selectedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 100,
+    );
+    setState(() {});
+  }
+
+  void _pickMultipleImagesFromGallery() async {
+    var images = await ImagePicker().pickMultiImage(
+      imageQuality: 100,
+      limit: 3,
+    );
+    selectedMultiImage.clear();
+
+    /// Temp solution for Android limit select images
+    // for (int i = 0; i < 3; i++) {
+    //   selectedMultiImage.add(images[i]);
+    // }
+    selectedMultiImage.addAll(images);
+    setState(() {});
   }
 
   @override
@@ -107,6 +143,47 @@ class _RegisterScreenState extends State<RegisterScreen> {
         child: ListView(
           padding: const EdgeInsets.all(10),
           children: [
+            const SizedBox(height: 20),
+            selectedImage == null
+                ? GestureDetector(
+                    onTap: _pickImageFromCamera,
+                    child: const CircleAvatar(
+                      radius: 100,
+                      backgroundImage: AssetImage(img3),
+                    ),
+                  )
+                : GestureDetector(
+                    onTap: _pickImageFromGallery,
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage: FileImage(
+                        File(selectedImage!.path),
+                      ),
+                    ),
+                  ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickMultipleImagesFromGallery,
+              child: const Text('Select Multiple Images'),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 150,
+              child: ListView.builder(
+                itemCount: selectedMultiImage.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return CircleAvatar(
+                    radius: 80,
+                    backgroundImage: FileImage(
+                      File(selectedMultiImage[index]!.path),
+                    ),
+                  );
+                },
+              ),
+            ),
             const SizedBox(height: 20),
             TextFormField(
               keyboardType: TextInputType.emailAddress,
@@ -420,6 +497,24 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
             ),
             const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text('Age'),
+            ),
+            Slider(
+              value: ageSliderValue,
+              max: 100,
+              divisions: 100,
+              min: 18,
+              label: ageSliderValue.round().toString(),
+              allowedInteraction: SliderInteraction.tapAndSlide,
+              secondaryActiveColor: Colors.red,
+              onChanged: (double newValue) {
+                ageSliderValue = newValue;
+                setState(() {});
+              },
+            ),
+            const SizedBox(height: 20),
             Row(
               children: [
                 Radio(
@@ -577,13 +672,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
               visualDensity: VisualDensity.compact,
               dense: true,
               subtitle: GestureDetector(
-                onTap: (){
+                onTap: () {
                   print('clicked');
                 },
-                onLongPress: (){
+                onLongPress: () {
                   print('onLongPress clicked');
                 },
-                onDoubleTap: (){
+                onDoubleTap: () {
                   print('onDoubleTap clicked');
                 },
                 child: Text(
@@ -602,13 +697,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ),
             const SizedBox(height: 20),
             InkWell(
-              onTap: (){
+              onTap: () {
                 print('inkwell on tap clicked');
               },
-              onLongPress: (){
+              onLongPress: () {
                 print('inkwell onLongPress clicked');
               },
-              onDoubleTap: (){
+              onDoubleTap: () {
                 print('inkwell onDoubleTap clicked');
               },
               child: Padding(
@@ -627,9 +722,31 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
             ),
             const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Add me to WhatsApp Updates'),
+                SizedBox(
+                  height: 25,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Switch(
+                      value: isWhatsAppUpdatesEnable,
+                      activeColor: Colors.green,
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      onChanged: (newValue) {
+                        isWhatsAppUpdatesEnable = newValue;
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                if(_validateRegisterForm()){
+                if (_validateRegisterForm()) {
                   print('success');
                   // APi call
                 }
